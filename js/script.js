3a9c095d1a6bf9b48b19a548df6058bbb8d4a718
Alex Padalka <alex.com.ua@gmail.com>
2019-02-27 09:26:44 +0700

    icons updated

---
+++ js/script.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function ($) {
+    $(".hamburger").click(function () {
+        $(".hamburger").toggleClass('is-active');
+        $(".menu").slideToggle(500);
+    });
+});
+
+
